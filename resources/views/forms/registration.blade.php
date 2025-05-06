c73527ea072300a1ffcf04ee8a8d8c8eeb74a3d4
atiqul-haque <atiqulhaque01@gmail.com>
2025-05-06 11:12:23 +0600

    Registration Form Added

---
+++ resources/views/forms/registration.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration</title>
+    <link rel="stylesheet" href="https://matcha.mizu.sh/matcha.css">
+</head>
+<body>
+    <h1>User Regsitration Form</h1>
+    <form action="">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username">
+
+        <label for="phone">Phone:</label>
+        <input type="text" id="phone" name="phone">
+
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email">
+
+        <br>
+        <input type="file" name="photo" id="">
+        <br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
